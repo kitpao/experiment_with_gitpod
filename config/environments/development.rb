a27fec3896ece42db8ec018d2286a6607810bd61
Eduardo Cruz <jecruxiz@gmail.com>
2020-05-11 22:56:36 +0000

    Adding and configuring Ruy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "3000-e80dd738-c791-428d-8b2b-c28c69e2f655.ws-us02.gitpod.io"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
