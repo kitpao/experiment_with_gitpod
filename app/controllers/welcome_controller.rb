59804b7448a39a3e9ac32d2e9cb0c192a4abd8dc
Eduardo Cruz <jecruxiz@gmail.com>
2020-05-12 21:57:43 +0000

    add about

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,9 @@ class WelcomeController < ApplicationController
   def home 
 
   end
+
+  def about
+
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
     get 'welcome/home' , to: 'welcome#home'
+    get 'welcome/about', to: 'welcome#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
