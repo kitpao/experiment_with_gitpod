69a2633542f1deb20e8f653b82a7394202f0343d
Eduardo Cruz <jecruxiz@gmail.com>
2020-05-13 21:31:21 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-    get 'welcome/home' , to: 'welcome#home'
-    get 'welcome/about', to: 'welcome#about'
+    b = 'ut'
+    c = 'it'
+    if 1 == 1
+      d = b
+    else
+      d = c
+    end
+    a = 'abo'+ d
+    root 'pages#home'
+    get a, to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
