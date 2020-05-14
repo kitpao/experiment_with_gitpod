50052ba22776828777b9c766a1e3b39c68bba48a
kitpao <kitpao91@gmail.com>
2020-05-14 22:36:05 +0000

    create new template for new, add resources for articles in routes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ApplicationRecord
     validates :title, length: { minimum: 3, maximum: 20 }
     validates :description, length: { minimum: 5, maximum: 200}
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       d = c
     end
     a = 'abo'+ d
+    resources :articles
     root 'pages#home'
     get a, to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
