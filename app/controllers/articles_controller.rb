5698959021420ca284fd61105c0e49aac65a27ad
kitpao <kitpao91@gmail.com>
2020-05-30 18:10:36 +0000

    add show view and errors message for new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
@@ -6,9 +11,15 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was succesfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
+
+  
 
   private
     def article_params
