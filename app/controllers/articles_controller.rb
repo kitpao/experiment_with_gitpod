a57455bd09376fc915b4eec78157c8f83969afde
kitpao <kitpao91@gmail.com>
2020-05-31 02:28:40 +0000

    Fix new form and add edit functionality to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,18 +8,31 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was succesfully created"
+      flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
     end
   end
 
-  
+  def update
+    #render plain: params[:article].inspect
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
 
   private
     def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-    b = 'ut'
-    c = 'it'
-    if 1 == 1
-      d = b
-    else
-      d = c
-    end
-    a = 'abo'+ d
-    resources :articles
-    root 'pages#home'
-    get a, to: 'pages#about'
+  resources :articles
+  root 'pages#home'
+  get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
